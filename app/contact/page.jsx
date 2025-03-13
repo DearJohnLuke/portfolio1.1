b5ac4cac37577c9400d32c3390b6bf4c37aed31a
RobustCode <robustcodeinc@gmail.com>
2025-03-13 17:01:52 +0200

    Update code

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -53,8 +53,7 @@ const Contact = () => {
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent">Let's work together</h3>
               <p className="text-white/60">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum
-                nihil sapiente pariatur id totam.
+              Looking forward to hearing about your next big idea.
               </p>
               {/* input */}
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
